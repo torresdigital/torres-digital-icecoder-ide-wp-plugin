515a083f89f4b1d0cb0ef8bcfdfda05f1c259fa9
Anderson Torres <atorresnh@gmail.com>
2019-08-19 01:05:15 -0300

    Update torres-digital-icecoder-ide-wp-plugin.php

--- torres-digital-icecoder-ide-wp-plugin.php
+++ torres-digital-icecoder-ide-wp-plugin.php
@@ -24,8 +24,8 @@ Author URI: https://facebook.com/torresdigital */
                 ),
             ));
             $admin_bar->add_menu( array(
-                'id'    => 'my-sub-item',
-                'parent' => 'my-item',
+                'id'    => 'icecoder-ide-sub-item',
+                'parent' => 'icecoder-ide',
                 'title' => 'My Sub Menu Item',
                 'href'  => '#',
                 'meta'  => array(
@@ -35,17 +35,93 @@ Author URI: https://facebook.com/torresdigital */
                 ),
             ));
             $admin_bar->add_menu( array(
-                'id'    => 'my-second-sub-item',
-                'parent' => 'my-item',
-                'title' => 'My Second Sub Menu Item',
-                'href'  => '#',
+                'id'    => 'icecoder-ide-second-sub-item',
+                'parent' => 'icecoder-ide',
+                'title' => 'Torres Digital -Sites → Lojas Virtuais e e-Commerce',
+                'href'  => 'https://www.torresdigital.tk/',
                 'meta'  => array(
-                    'title' => __('My Second Sub Menu Item'),
+                    'title' => __('Torres Digital -Sites → Lojas Virtuais e e-Commerce'),
                     'target' => '_blank',
                     'class' => 'my_menu_item_class'
                 ),
             ));
         }
+
+
+
+/*
+* add a group of links under a parent link
+*/
+
+// Add a parent shortcut link
+
+function custom_toolbar_link($wp_admin_bar) {
+    $args = array(
+        'id' => 'wpbeginner',
+        'title' => 'WPBeginner',
+        'href' => 'https://www.wpbeginner.com',
+        'meta' => array(
+            'class' => 'wpbeginner',
+            'title' => 'Visit WPBeginner'
+            )
+    );
+    $wp_admin_bar->add_node($args);
+
+// Add the first child link
+
+    $args = array(
+        'id' => 'wpbeginner-guides',
+        'title' => 'WPBeginner Guides',
+        'href' => 'https://www.wpbeginner.com/category/beginners-guide/',
+        'parent' => 'wpbeginner',
+        'meta' => array(
+            'class' => 'wpbeginner-guides',
+            'title' => 'Visit WordPress Beginner Guides'
+            )
+    );
+    $wp_admin_bar->add_node($args);
+
+// Add another child link
+$args = array(
+        'id' => 'wpbeginner-tutorials',
+        'title' => 'WPBeginner Tutorials',
+        'href' => 'https://www.wpbeginner.com/category/wp-tutorials/',
+        'parent' => 'wpbeginner',
+        'meta' => array(
+            'class' => 'wpbeginner-tutorials',
+            'title' => 'Visit WPBeginner Tutorials'
+            )
+    );
+    $wp_admin_bar->add_node($args);
+
+// Add a child link to the child link
+
+$args = array(
+        'id' => 'wpbeginner-themes',
+        'title' => 'WPBeginner Themes',
+        'href' => 'https://www.wpbeginner.com/category/wp-themes/',
+        'parent' => 'wpbeginner-tutorials',
+        'meta' => array(
+            'class' => 'wpbeginner-themes',
+            'title' => 'Visit WordPress Themes Tutorials on WPBeginner'
+            )
+    );
+    $wp_admin_bar->add_node($args);
+
+}
+
+add_action('admin_bar_menu', 'custom_toolbar_link', 999);
+
+ add_action( 'admin_menu', 'linked_url' );
+    function linked_url() {
+    add_menu_page( 'linked_url', 'External link', 'read', 'my_slug', '', 'dashicons-text', 1 );
+    }
+
+    add_action( 'admin_menu' , 'linkedurl_function' );
+    function linkedurl_function() {
+    global $menu;
+    $menu[1][2] = "http://www.example.com";
+    }
 
 /* Style */
 function wpse_load_plugin_css() {
