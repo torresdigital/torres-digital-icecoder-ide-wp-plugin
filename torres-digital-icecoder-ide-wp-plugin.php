882d590f031a7d73fe9872dd9afa0c258a3dfc1c
Anderson Torres <atorresnh@gmail.com>
2019-08-25 03:15:31 -0300

    Commits on 25 Aug - 3:15 AM

--- torres-digital-icecoder-ide-wp-plugin.php
+++ torres-digital-icecoder-ide-wp-plugin.php
@@ -16,7 +16,7 @@ Author URI: https://facebook.com/torresdigital */
             $admin_bar->add_menu( array(
                 'id'    => 'icecoder-ide',
                 'title' => ' ❉ ICECoder IDE - Login',
-                'href'  => '/wp-content/plugins/torres-digital-icecoder-ide-wp-plugin/ICEcoder/',
+                'href'  => '/wp-content/plugins/torres-digital-icecoder-ide-wp-plugin-master/ICEcoder/',
                 'meta'  => array(
                     'title' => __('ICECoder IDE - Login'),
                     'target' => '_blank',
@@ -51,19 +51,13 @@ Author URI: https://facebook.com/torresdigital */
 
 /*
 * Font AWesome
-*/ /*---Font Awesome, by Torres Digital */
-    function theme_enqueue_styles() {
-            $parent_style = 'parent-style';
-            wp_enqueue_style( $parent_style, get_template_directory_uri() . 'css/style.css' );
-            wp_enqueue_style( 'my-child-fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
-            wp_enqueue_style( 'child-style',
-                get_stylesheet_directory_uri() . 'css/style.css',
-                array( $parent_style )
-            );
-        }
+*/
+function wmpudev_enqueue_icon_stylesheet() {
+	wp_register_style( 'fontawesome', 'http:////maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'fontawesome');
+}
+add_action( 'wp_enqueue_scripts', 'wmpudev_enqueue_icon_stylesheet' );
 // Add a parent shortcut link
-
-
 
 
 /* Style */
