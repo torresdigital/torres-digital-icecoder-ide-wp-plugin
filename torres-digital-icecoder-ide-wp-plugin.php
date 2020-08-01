d2872d457baa3c5855d910d0299f3279d80844bc
atorresbr <atorresnh@gmail.com>
2020-08-01 17:29:25 -0300

    Update torres-digital-icecoder-ide-wp-plugin.php

--- torres-digital-icecoder-ide-wp-plugin.php
+++ torres-digital-icecoder-ide-wp-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Torres Digital ICECoder IDE WP Plugin
 Plugin URI: http://torresdigital.com.br/
 Description: ❉ This plugin allows the <strong>ICECoder IDE</strong> to be accessed directly through the Wordpress Panel. Simple as that, simple as life should be. Source Code on GitHub https://github.com/icecoder/ICEcoder | ICECoder IDE Site - https://icecoder.net/
-Version: 1.o - 2020
+Version: 1.0 - 2020
 Author: Torres Digital -Sites → Lojas Virtuais e e-Commerce
 Author URI: https://facebook.com/torresdigital */
 
@@ -16,7 +16,7 @@ Author URI: https://facebook.com/torresdigital */
             $admin_bar->add_menu( array(
                 'id'    => 'icecoder-ide',
                 'title' => ' ❉ ICECoder IDE - Login',
-                'href'  => '/wp-content/plugins/torres-digital-icecoder-ide-wp-plugin/ICEcoder/lib/login.php',
+                'href'  => '/wp-content/plugins/torres-digital-icecoder-ide-wp-plugin-master/ICEcoder/lib/login.php',
                 'meta'  => array(
                     'title' => __('* ICECoder IDE - Login'),
                     'target' => '_blank',
